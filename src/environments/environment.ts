4baa1971825eb2648d2d630c42e89f9116c97ce7
dimitrisfinas <88084804+dimitrisfinas@users.noreply.github.com>
2022-11-24 10:00:01 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: false,
   title: 'Local Environment Heading',
-  LIGHTSTEP_ACCESS_TOKEN:
-    $LIGHTSTEP_ACCESS_TOKEN,
-  OTEL_EXPORTER_OTLP_ENDPOINT: 'https://ingest.lightstep.com:443/traces/otlp/v0.9',
+  LIGHTSTEP_ACCESS_TOKEN: $LIGHTSTEP_ACCESS_TOKEN,
+  OTEL_EXPORTER_OTLP_ENDPOINT: $OTEL_EXPORTER_OTLP_ENDPOINT,
   OTEL_SERVICE_NAME: 'angular-frontend',
 };
