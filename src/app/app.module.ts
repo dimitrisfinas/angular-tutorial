4bd4cc6c77ebfce00fa13c02ba3dbdf611d644aa
dimitrisfinas <88084804+dimitrisfinas@users.noreply.github.com>
2022-11-24 15:30:18 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
@@ -13,6 +12,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
 
+// OpenTelemetry tracing wrapper
 import FrontendTracer from '../utils/telemetry/FrontendTracer';
 if (typeof window !== 'undefined') FrontendTracer();
 
