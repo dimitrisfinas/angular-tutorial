7126d953fd7258522cab06c67dc6ab50e1c9e4dc
Dimitris Finas <dfinas@gmail.com>
2022-11-24 11:49:36 +0100

    add env file generator

---
+++ src/environments/createEnvFile.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+SCRIPTDIR=$(dirname "$0")
+cat > ./$SCRIPTDIR/environment.ts <<EOF
+export const environment = {
+  production: false,
+  title: 'Local Environment Heading',
+  LIGHTSTEP_ACCESS_TOKEN: '$LIGHTSTEP_ACCESS_TOKEN',
+  OTEL_EXPORTER_OTLP_ENDPOINT: '$OTEL_EXPORTER_OTLP_ENDPOINT',
+  OTEL_SERVICE_NAME: 'angular-frontend',
+};
+EOF)
