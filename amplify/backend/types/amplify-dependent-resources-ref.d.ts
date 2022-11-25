10c95b19a70146db1d57c2cb455b2ce7c5d35ea9
Dimitris Finas <dfinas@gmail.com>
2022-11-25 11:04:56 +0100

    add amplify layer + Rest API + Function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "angulartutorialfuncHello": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "angulartutorialApiHello": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
